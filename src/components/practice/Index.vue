c81bbec2bd1e9d0e5a5a616f061c8bc4ce174fd4
Samuel <samuel@obdesign.com.tw>
2020-07-22 18:10:26 +0800

    新增practice組件

---
+++ src/components/practice/Index.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+    
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+    @Prop() private questions!: Array<object>;
+    @Prop() private total!: number;
+    @Prop() private callback!: Function;
+    private qNum = 0;
+    private name = '';
+}
+</script>
+<style lang="scss">
+.practice {
+    .custom-control {
+        .custom-style-label {
+            &::before,
+            &::after {
+                top: 10px;
+            }
+        }
+    }
+    .btn {
+        margin-top: 20px;
+    }
+}
+</style>
