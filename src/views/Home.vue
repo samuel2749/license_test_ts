3c44941cd24faf87a0c044ef512765e02c660fea
Samuel <samuel@obdesign.com.tw>
2020-08-07 17:54:48 +0800

    新增GSAP套件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 	.home.container(:class="{'text-center': !isMobile}")
-		h1.text-center.mb-5 資四甲 證照測驗
+		h1.text-center.mb-5(ref="test") 資四甲 證照測驗
 		router-link.btn.btn-lg.btn-success(:class="{'btn-block': isMobile}" to="/normal/bulldozer") 推土機 測驗
 		router-link.btn.btn-lg.btn-success(:class="{'btn-block': isMobile, 'ml-2': !isMobile}" to="/normal/mkp") MKP 測驗
 		router-link.btn.btn-lg.btn-success(:class="{'btn-block': isMobile, 'ml-2': !isMobile}" to="/normal/ebc") EBC 測驗
@@ -9,11 +9,14 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
+import gsap from 'gsap';
 @Component
 export default class Home extends Vue {
     private get isMobile() {
         return this.$store.state.isMobile;
+    }
+    mounted() {
+        gsap.from(this.$refs.test, { duration: 1, x: 100 });
     }
 }
 </script>
